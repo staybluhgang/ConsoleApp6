4acc4c97cbad9ef9dfe98ea3d237078cfdca30fb
d3baf <korobka.yaroslav@yandex.ru>
2024-03-26 20:17:21 +0300

    класс расширения print

--- ConsoleApp6/Program.cs
+++ ConsoleApp6/Program.cs
@@ -1,2 +1,1 @@
-﻿
-Console.WriteLine("Hello, World!");
+﻿using ConsoleApp6;
---
+++ ConsoleApp6/extension.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp6
+{
+    static class Extension
+    {
+        public static T[] Print<T>(this T[] array) { 
+          foreach (var item in array)
+               Console.Write(item + "\t");
+           Console.WriteLine();
+            return array;
+
+        }
+        public static T[] Print<T>(this T[] item)
+        {
+            Console.WriteLine(item);
+            return item;
+
+        }
+
+    }
+}
